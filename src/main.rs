1d17a204372517685d436fdac904a32bc622ffe4
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-11-07 14:00:19 +0200

    Add unidiomatic Hello, World! via VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ use core::panic::PanicInfo;
 
 entry_point!(_kernel_entry);
 
+static HELLO: &[u8] = b"Hello, World!";
+
 #[no_mangle]
 pub fn _kernel_entry(boot_info: &'static BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
