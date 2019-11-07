db686366ca87280444e1e008f161c58db295c978
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-11-07 13:49:04 +0200

    Add bootloader as dependency

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![no_std]
 #![no_main]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+entry_point!(_kernel_entry);
+
+#[no_mangle]
+pub fn _kernel_entry(boot_info: &'static BootInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _kernel_entry_point() -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
