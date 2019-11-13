2cf7b56feed9ad0e61469cdc4645429ea98383cb
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-11-13 12:20:06 +0200

    Print PanicInfo on panic

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,17 @@ use core::panic::PanicInfo;
 entry_point!(_kernel_entry_point);
 
 #[no_mangle]
+#[allow(unreachable_code)]
 pub fn _kernel_entry_point(_boot_info: &'static BootInfo) -> ! {
     println!("Hello, World{}", "!");
+    panic!("Whoopsie ;)");
 
+    // We'll never get this far, thus #[allow(unreachable_code)]
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
