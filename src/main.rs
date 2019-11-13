c9b230ecbea8a45fa8416d2978c7da0785bcc16f
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-11-13 13:29:35 +0200

    Add a custom test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -13,10 +16,19 @@ entry_point!(_kernel_entry_point);
 pub fn _kernel_entry_point(_boot_info: &'static BootInfo) -> ! {
     // The 'ö' here demostrates how non-ASCII characters are handled
     println!("Hello, World{}", "! ö");
-    panic!("Whoopsie ;)");
 
-    // We'll never get this far, thus #[allow(unreachable_code)]
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
 
 #[panic_handler]
