3047925de9a1f21a5f6849e6a0776359ecb827b4
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-11-14 22:36:41 +0200

    Add serial device and test output via uart_16550

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 use bootloader::{entry_point, BootInfo};
@@ -25,16 +26,26 @@ pub fn _kernel_entry_point(_boot_info: &'static BootInfo) -> ! {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
     loop {}
 }
 
@@ -59,7 +70,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(true, true);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
