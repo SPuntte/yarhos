b1ea3631fbab9d1b59c94e553a20425bb5f951d2
Pontus Lundström <pontus.lundstrom@gmail.com>
2023-05-25 15:03:36 +0300

    Add LLDB config generation to VSCode script

--- scripts/gen_vscode_debug_config.py
+++ scripts/gen_vscode_debug_config.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import functools
 import json
 import re
 import subprocess
 
 
+@functools.cache
 def cargo_get_target():
     cargo_output = subprocess.run(
         ["cargo", "config", "get", "-Z", "unstable-options", "--format", "json", "build.target"],
@@ -13,6 +15,7 @@ def cargo_get_target():
     return json.loads(cargo_output)["build"]["target"].removesuffix(".json")
 
 
+@functools.cache
 def cargo_get_package_name():
     return subprocess.run(["cargo", "pkgid"],
                           capture_output=True).stdout.decode().rsplit("/", 1)[-1].split("#")[0]
@@ -70,27 +73,36 @@ def replace_or_append_configuration(configurations, configuration):
     replace_or_append(configurations, configuration, "name")
 
 
-def gen_tasks_json(tasks, integration_tests):
-    TASK_TEMPLATE = {
-        "type": "shell",
-        "hide": True,
-        "dependsOn": [
-            "symlink-test-executables"
-        ],
-        "dependsOrder": "sequence",
-        "isBackground": True,
-        "problemMatcher": {
-            "pattern": {
-                "regexp": "________"
-            },
-            "background": {
-                "activeOnStart": True,
-                "beginsPattern": "Building",
-                "endsPattern": "Running"
-            }
+TASK_LABEL_PREFIX = "debug-"
+TASK_TEMPLATE = {
+    "type": "shell",
+    "hide": True,
+    "dependsOn": [
+        "symlink-test-executables"
+    ],
+    "dependsOrder": "sequence",
+    "isBackground": True,
+    "problemMatcher": {
+        "pattern": {
+            "regexp": "________"
+        },
+        "background": {
+            "activeOnStart": True,
+            "beginsPattern": "Building",
+            "endsPattern": "Running"
         }
     }
+}
 
+
+def gen_cargo_task(label, cargo_cmd):
+    task = TASK_TEMPLATE.copy()
+    task["label"] = f"{TASK_LABEL_PREFIX}{label}"
+    task["command"] = f"cargo {cargo_cmd} -- -s -S"
+    return task
+
+
+def gen_tasks_json(tasks, integration_tests):
     # Task: Create predictable symlinks to test executables
     symlink_task = {
         "label": "symlink-test-executables",
@@ -105,82 +117,124 @@ def gen_tasks_json(tasks, integration_tests):
     }
     replace_or_append_task(tasks, symlink_task)
 
-    # Task: `cargo run` with arguments to wait for GDB attach
-    gdb_run = TASK_TEMPLATE.copy()
+    # Task: `cargo run` with arguments to wait for debugger attach
+    debug_run = gen_cargo_task("run", "run")
     # No symlink for the default binary
-    del gdb_run["dependsOn"]
-    del gdb_run["dependsOrder"]
-    gdb_run["command"] = "cargo run -- -s -S"
-    gdb_run["label"] = "gdb-run"
-    replace_or_append_task(tasks, gdb_run)
+    del debug_run["dependsOn"]
+    del debug_run["dependsOrder"]
+    replace_or_append_task(tasks, debug_run)
 
-    # Task: `cargo test` binary crate (GDB)
-    gdb_unittests_bin = TASK_TEMPLATE.copy()
+    # Task: `cargo test` binary crate
     package_name = cargo_get_package_name()
-    gdb_unittests_bin["command"] = f"cargo test --bin {package_name} -- -s -S"
-    gdb_unittests_bin["label"] = "gdb-unittests-bin"
-    replace_or_append_task(tasks, gdb_unittests_bin)
+    debug_unittests_bin = gen_cargo_task("unittests-bin", f"test --bin {package_name}")
+    replace_or_append_task(tasks, debug_unittests_bin)
 
-    # Task: `cargo test` library crate (GDB)
-    gdb_unittests_lib = TASK_TEMPLATE.copy()
-    gdb_unittests_lib["command"] = "cargo test --lib -- -s -S"
-    gdb_unittests_lib["label"] = "gdb-unittests-lib"
-    replace_or_append_task(tasks, gdb_unittests_lib)
+    # Task: `cargo test` library crate
+    debug_unittests_lib = gen_cargo_task("unittests-lib", "test --lib")
+    replace_or_append_task(tasks, debug_unittests_lib)
 
     for test in integration_tests:
-        # Task: `cargo test` integration test (GDB)
-        gdb_integration_test = TASK_TEMPLATE.copy()
-        gdb_integration_test["command"] = f"cargo test --test {test} -- -s -S"
-        gdb_integration_test["label"] = f"gdb-test-{test}"
-        replace_or_append_task(tasks, gdb_integration_test)
+        # Task: `cargo test` integration test
+        debug_integration_test = gen_cargo_task(f"test-{test}", f"test --test {test}")
+        replace_or_append_task(tasks, debug_integration_test)
 
 
-def gen_launch_json(configurations, integration_tests):
-    CONFIGURATION_TEMPLATE = {
-        "type": "gdb",
-        "gdbpath": "rust-gdb",
-        "request": "attach",
-        "target": ":1234",
-        "remote": True,
-        "stopAtEntry": "_start",
-        "internalConsoleOptions": "openOnSessionStart",
-        "cwd": "${workspaceRoot}",
-    }
+CONFIGURATION_NAME_SUFFIX_GDB = " (GDB+QEMU)"
+CONFIGURATION_TEMPLATE_GDB = {
+    "type": "gdb",
+    "gdbpath": "rust-gdb",
+    "request": "attach",
+    "target": ":1234",
+    "remote": True,
+    "stopAtEntry": "_start",
+    "internalConsoleOptions": "openOnSessionStart",
+    "cwd": "${workspaceRoot}",
+}
 
-    # Config: attach to OS binary (running in QEMU)
-    gdb_run = CONFIGURATION_TEMPLATE.copy()
-    target = cargo_get_target()
+
+def gen_gdb_config(name, executable, pre_launch_task):
+    debug_target_dir = f"target/{cargo_get_target()}/debug"
+    config = CONFIGURATION_TEMPLATE_GDB.copy()
+    config["executable"] = f"{debug_target_dir}/{executable}"
+    config["preLaunchTask"] = f"{TASK_LABEL_PREFIX}{pre_launch_task}"
+    config["name"] = f"{name}{CONFIGURATION_NAME_SUFFIX_GDB}"
+    return config
+
+
+CONFIGURATION_NAME_SUFFIX_LLDB = " (LLDB+QEMU)"
+CONFIGURATION_TEMPLATE_LLDB = {
+    "type": "lldb",
+    "request": "custom",
+    # Need to continue on attach (see QEMU man page, option '-S')
+    "processCreateCommands": ["gdb-remote localhost:1234", "c"],
+}
+
+
+def gen_lldb_config(name, executable, pre_launch_task):
+    debug_target_dir = f"target/{cargo_get_target()}/debug"
+    config = CONFIGURATION_TEMPLATE_LLDB.copy()
+    config["targetCreateCommands"] = [
+        f"target create ${{workspaceRoot}}/{debug_target_dir}/{executable}",
+        # Set a breakpoint at the program entry
+        "b _start"
+    ]
+    config["preLaunchTask"] = f"{TASK_LABEL_PREFIX}{pre_launch_task}"
+    config["name"] = f"{name}{CONFIGURATION_NAME_SUFFIX_LLDB}"
+    return config
+
+
+def gen_launch_json(configurations, integration_tests, *, variants=["gdb", "lldb"]):
     package_name = cargo_get_package_name()
-    gdb_run["executable"] = f"target/{target}/debug/{package_name}"
-    gdb_run["preLaunchTask"] = "gdb-run"
-    gdb_run["name"] = f"{package_name} (GDB+QEMU)"
-    replace_or_append_configuration(configurations, gdb_run)
 
-    # Config: attach to OS binary unittests
-    gdb_unittests_bin = CONFIGURATION_TEMPLATE.copy()
-    target = cargo_get_target()
-    package_name = cargo_get_package_name()
-    gdb_unittests_bin["executable"] = f"target/{target}/debug/{package_name}-unittests-bin"
-    gdb_unittests_bin["preLaunchTask"] = "gdb-unittests-bin"
-    gdb_unittests_bin["name"] = "unittests [bin] (GDB+QEMU)"
-    replace_or_append_configuration(configurations, gdb_unittests_bin)
+    if "gdb" in variants:
+        # Config: attach to OS binary (running in QEMU)
+        gdb_run = gen_gdb_config(package_name, package_name, "run")
+        replace_or_append_configuration(configurations, gdb_run)
 
-    # Config: attach to OS library unittests
-    gdb_unittests_lib = CONFIGURATION_TEMPLATE.copy()
-    target = cargo_get_target()
-    package_name = cargo_get_package_name()
-    gdb_unittests_lib["executable"] = f"target/{target}/debug/{package_name}-unittests-lib"
-    gdb_unittests_lib["preLaunchTask"] = "gdb-unittests-lib"
-    gdb_unittests_lib["name"] = "unittests [lib] (GDB+QEMU)"
-    replace_or_append_configuration(configurations, gdb_unittests_lib)
+        # Config: attach to OS binary unittests
+        gdb_unittests_bin = gen_gdb_config(
+            "unittests [bin]",
+            f"{package_name}-unittests-bin",
+            "unittests-bin")
+        replace_or_append_configuration(configurations, gdb_unittests_bin)
 
-    for test in integration_tests:
-        # Config: attach to integration test `test`
-        gdb_integration_test = CONFIGURATION_TEMPLATE.copy()
-        gdb_integration_test["executable"] = f"target/{target}/debug/{package_name}-test-{test}"
-        gdb_integration_test["preLaunchTask"] = f"gdb-test-{test}"
-        gdb_integration_test["name"] = f"tests/{test} (GDB+QEMU)"
-        replace_or_append_configuration(configurations, gdb_integration_test)
+        # Config: attach to OS library unittests
+        gdb_unittests_lib = gen_gdb_config(
+            "unittests [lib]",
+            f"{package_name}-unittests-lib",
+            "unittests-lib")
+        replace_or_append_configuration(configurations, gdb_unittests_lib)
+
+        for test in integration_tests:
+            # Config: attach to integration test `test`
+            gdb_integration_test = gen_gdb_config(
+                f"tests/{test}", f"{package_name}-test-{test}", f"test-{test}")
+            replace_or_append_configuration(configurations, gdb_integration_test)
+
+    if "lldb" in variants:
+        # Config: attach to OS binary (running in QEMU)
+        lldb_run = gen_lldb_config(package_name, package_name, "run")
+        replace_or_append_configuration(configurations, lldb_run)
+
+        # Config: attach to OS binary unittests
+        lldb_unittests_bin = gen_lldb_config(
+            "unittests [bin]",
+            f"{package_name}-unittests-bin",
+            "unittests-bin")
+        replace_or_append_configuration(configurations, lldb_unittests_bin)
+
+        # Config: attach to OS library unittests
+        lldb_unittests_lib = gen_lldb_config(
+            "unittests [lib]",
+            f"{package_name}-unittests-lib",
+            "unittests-lib")
+        replace_or_append_configuration(configurations, lldb_unittests_lib)
+
+        for test in integration_tests:
+            # Config: attach to integration test `test`
+            lldb_integration_test = gen_lldb_config(
+                f"tests/{test}", f"{package_name}-test-{test}", f"test-{test}")
+            replace_or_append_configuration(configurations, lldb_integration_test)
 
 
 def main():
